baf37d5e537b7ada15819a3f2a3bc50e908be5b5
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-04-17 13:07:46 -0500

    more w2 ocr eval dataset

--- benchmarks/ocr.eval.ts
+++ benchmarks/ocr.eval.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import { createOpenAI } from '@ai-sdk/openai'
 import { generateObject } from 'ai'
 import { JSONDiff } from 'autoevals'
@@ -5,13 +6,19 @@ import { evalite } from 'evalite'
 import { traceAISDKModel } from 'evalite/ai-sdk'
 import { models } from '../models'
 import { z } from 'zod'
+import { createOpenAICompatible } from '@ai-sdk/openai-compatible'
 
+// console.log(process.env.AI_GATEWAY_URL)
 
 const router = createOpenAI({
   // custom settings, e.g.
-  compatibility: 'strict', // strict mode, enable when using the OpenAI API
-  apiKey: process.env.AI_GATEWAY_TOKEN,
-  baseURL: process.env.AI_GATEWAY_URL,
+  // compatibility: 'strict', // strict mode, enable when using the OpenAI API
+  // compatibility: 'compatible',
+
+// const router = createOpenAICompatible({
+//   name: 'OpenRouter',
+  apiKey: process.env.AI_GATEWAY_TOKEN!,
+  baseURL: process.env.AI_GATEWAY_URL!,
   headers: {
     "HTTP-Referer": "https://workflows.do", // Optional. Site URL for rankings on openrouter.ai.
     "X-Title": "Workflows.do Business-as-Code", // Optional. Site title for rankings on openrouter.ai.
@@ -42,22 +49,24 @@ const expected = JSON.stringify({
 })
 
 
-const images = [1,2,3,4,5]
-const blur = [0,1,2,3]
+// const images = [1,2,3,4,5]
+const images = [3,4,5]
+// const blurs = [0,1,2,3]
+const blurs = [0]
 const resolution = [512, 768, 1536, 2000, 3072]
-
+  
 for (const model of models) {
   evalite('W2 OCR - ' + model, {
     data: async () => {
       const permutations = [];
       for (const img of images) {
-        for (const b of blur) {
+        for (const blur of blurs) {
           for (const res of resolution) {
             permutations.push({
               input: { 
-                image: `https://github.com/drivly/benchmarks.do/blob/main/datasets/data-extraction/w2/w2_img${img}_blur_${b}_res${res}.jpg?raw=true`,
+                image: `https://github.com/drivly/benchmarks.do/blob/main/datasets/data-extraction/w2/w2_img${img}_blur${blur}_res${res}.jpg?raw=true`,
                 imageNumber: img,
-                blurLevel: b,
+                blurLevel: blur,
                 resolution: res
               },
               expected,
@@ -69,12 +78,13 @@ for (const model of models) {
     },
     task: async (input) => {
       const result = await generateObject({
-        model: traceAISDKModel(router(model, { structuredOutputs: true })),
+        // model: traceAISDKModel(router(model, { structuredOutputs: true })),
+        model: router(model, { structuredOutputs: true }),
         messages: [
           {
             role: 'user',
             content: [
-              { type: 'text', text: 'Extract the data from the image.' },
+              { type: 'text', text: 'Extract the data from the image. Use 0 or "" for missing values. Do not redact PII/SSN or make up anything you do not see.' },
               { type: 'image', image: input.image },
             ],
           },
@@ -103,8 +113,15 @@ for (const model of models) {
           employerZip: z.string(),
           taxYear: z.string(),
         }),
+        providerOptions: {
+          provider: {
+            require_parameters: true,
+          },
+        }
       })
   
+      console.log(model, input.image, result.object)
+
       return result.object
     },
     scorers: [JSONDiff],
